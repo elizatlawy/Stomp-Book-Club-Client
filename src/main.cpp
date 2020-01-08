a56cae710646d03f1ad1d8cfb8589242a180450b
zatlawy@wincs.cs.bgu.ac.il <zatlawy@post.bgu.ac.il>
2020-01-08 22:08:19 +0200

    userData

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by zatlawy@wincs.cs.bgu.ac.il on 08/01/2020.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "test" << endl;
+    return 0;
+}
