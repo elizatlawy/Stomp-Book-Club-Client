4dcf174ff90b5c8a1d0c2c70df8713dfb72414a2
zatlawy@wincs.cs.bgu.ac.il <zatlawy@post.bgu.ac.il>
2020-01-08 17:29:49 +0200

    cmake

---
+++ include/StompBookClubClient.h
@@ -0,0 +1,15 @@
+//
+// Created by zatlawy@wincs.cs.bgu.ac.il on 08/01/2020.
+//
+
+#ifndef BOOST_ECHO_CLIENT_STOMPBOOKCLUBCLIENT_H
+#define BOOST_ECHO_CLIENT_STOMPBOOKCLUBCLIENT_H
+
+
+class StompBookClubClient {
+
+
+};
+
+
+#endif //BOOST_ECHO_CLIENT_STOMPBOOKCLUBCLIENT_H
